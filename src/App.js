216e8d0fe5a3bd988c83b0b8db8f31d17df1f3ee
Nercruiser002 <aram2008tuticorin@gmail.com>
2025-07-24 11:23:55 +0530

    added a txt file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import './App.css'
-import {Routes, Route } from 'react-route-dom'
-import Login from  './Pages/Login'
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+      
     </div>
   )
 }
