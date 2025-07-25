a1582c9ad946d527bf3f1dc6bd544a47aeadfaca
Nercruiser002 <aram2008tuticorin@gmail.com>
2025-07-25 10:50:13 +0530

    added login page

---
+++ src/Pages/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <h3>Login</h3>
+        <div>
+          <h2>Username</h2>
+          <input />
+          <h2>User-email</h2>
+          <input />
+        </div>
+        <div>
+          <h2>Password</h2>
+          <input />
+        </div>
+        <div><p>Forgot Password<button>Click Here</button></p></div>
+        <div><p>Wanna Use e-mail?<button>Click Here</button></p></div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
